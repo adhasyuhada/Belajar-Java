e77da58caddd9f0380c2ad00927e619c84a1032b
adha <adha.teknik@gmail.com>
2016-10-29 01:26:37 +0700

    membuat for loop

---
+++ src/ForLoop.java
@@ -0,0 +1,12 @@
+public class ForLoop{
+	public static void main(String[] args){
+	//membuat variabel
+	String nama = "Hasan";
+	int umur = 26;
+
+	//membuat perulangan for (nilai awal; perbandingan perulangan; penambahan perulangan;)
+	for(int i = 0; i < 4; i++ ){
+		System.out.println("Nama saya " + nama + " Umur saya " + umur);
+	}
+	}
+}
