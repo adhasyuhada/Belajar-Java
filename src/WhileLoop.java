363a893eb2bd06fe27a70f4d83890ed41cf81004
adha <adha.teknik@gmail.com>
2016-10-29 09:44:00 +0700

    added WhileLoop

---
+++ src/WhileLoop.java
@@ -0,0 +1,15 @@
+public class WhileLoop{
+	public static void main(String[] xx){
+	//initialize variabel
+	int count = 1;
+
+	//while loop
+	while( count < 10){
+		//print count if < 10 from count++
+		// count++ added 1 after the value initialize variable int count = 1 printed first
+		System.out.println("Count is "+ count);
+		count++;
+	}		
+	}
+
+}
