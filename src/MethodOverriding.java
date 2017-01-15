653f77ef27d4c390e8156c6f04eee7801863bf3b
adha <adha.teknik@yahoo.com>
2017-01-15 15:10:07 +0700

    Update MethodOverriding.java

--- src/MethodOverriding.java
+++ src/MethodOverriding.java
@@ -1,16 +1,25 @@
-class SprClass{
-	void run(){
-		System.out.println("Invoke method run");
+class SprClass{ //parent class
+	int id = 11; //instance variable
+	
+	int setId(){ //method set mengembalikan nilai id = 11
+            return id;
 	}
+	
 }
-
-	class MethodOverriding extends SprClass{
-	void run(){
-			System.out.println("Invoke method overriding");
-		}
-	public static void main(String[] args) {
-		MethodOverriding pr = new MethodOverriding();
-		pr.run();
+	class MethodOverriding extends SprClass{ //inheritance class SprClass
+	int id = 22; //instance variable
+		
+	int setId(int id){ //method set mengembalikan nilai id = 11 diambil dari class parent dengan kata kunci super
+            return super.id; //kata kunci super mengacu ke id yang ada di parent class
+	}
+        
+	void printId(){ //method print untuk mencetak hasil dari overriding dengan kata kunci super
+		System.out.println(super.setId()); //mengambil nilai id = 11 dari class parent SprClass
+	}
+        
+	public static void main(String[] args) { //main method
+		MethodOverriding pr = new MethodOverriding(); //membuat objek baru pr
+		pr.printId(); //menjalankan method printId di dalam class MethodOverriding
 		
 	}
 }
