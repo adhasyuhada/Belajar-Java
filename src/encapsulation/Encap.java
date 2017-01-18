01f174f01ab9c25fa0e69940f7e3a17223ff7822
adha <adha.teknik@yahoo.com>
2017-01-18 15:12:17 +0700

    Create Encap.java

---
+++ src/encapsulation/Encap.java
@@ -0,0 +1,32 @@
+class Data{
+    private String name; //variabl private hanya bisa diakses di classnya sendiri
+    private int nim; //variable private tidak bisa diakses di luar class
+    
+    public void setNim(int yournim){ //method return public untuk user dari luar kelas agar dapat mengakses memberikan nilai pada variable private 
+        nim = yournim; //nilai parameter method sama menjadi nilai private 
+    }
+    
+    public void setName(String yourname){
+        name = yourname;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getNim(){
+        return nim;
+    }
+
+}
+
+public class Encap {
+    public static void main(String[] args){
+        Data obj =  new Data();
+        obj.setName("adha");
+        obj.setNim(12345);
+        System.out.println(obj.getName());
+        System.out.println(obj.getNim());
+    }
+    
+}
