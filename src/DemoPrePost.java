5a290a87d38ac256bde673243230b9f16e054813
adhasyuhada <adha.teknik@gmail.com>
2017-03-27 15:26:48 +0700

    add gitignore

---
+++ src/DemoPrePost.java
@@ -0,0 +1,14 @@
+class DemoPrePost{
+	public static void main(String[] args){
+		int i = 1;
+		int j = 1;		
+		System.out.println("i++ " + i++);
+		System.out.println("hasi dari postfix i++ : " +i);
+		System.out.println("++i " + ++i);
+		System.out.println("hasil dari prefix ++i : " + i);
+		System.out.println("--i " + --i);
+                System.out.println("hasil dari prefix --i : " + i);
+		System.out.println("i-- " + i--);
+                System.out.println("hasil dari postfix i-- : " + i--);
+	}	
+}
